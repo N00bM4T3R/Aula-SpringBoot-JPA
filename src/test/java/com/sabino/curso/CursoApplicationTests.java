f2be7a1b970e5541fbd625e28b03905e9a867f49
N00bM4T3R <sabinolbn1810@gmail.com>
2025-05-02 14:01:25 -0300

    inicio do projeto Spring

---
+++ src/test/java/com/sabino/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabino.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
