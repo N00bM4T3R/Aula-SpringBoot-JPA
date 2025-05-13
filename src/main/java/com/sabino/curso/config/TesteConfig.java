453c1708d98a2aea0759763b7e32e70c5603b5f6
N00bM4T3R <sabinolbn1810@gmail.com>
2025-05-13 10:38:19 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/sabino/curso/config/TesteConfig.java
+++ src/main/java/com/sabino/curso/config/TesteConfig.java
@@ -43,8 +43,6 @@ public class TesteConfig implements CommandLineRunner {
 		Category cat2 = new Category(0l, "Books");
 		Category cat3 = new Category(0l, "Computers");
 		
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		
 		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
@@ -52,6 +50,18 @@ public class TesteConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat2);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		User u1 = new User(0L, "Maria Brown", "maria@gmail.com", "988888888", "123456");
