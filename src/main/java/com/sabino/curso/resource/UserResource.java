94c9f332536212e6a3153907f1b4da09c172d05c
N00bM4T3R <sabinolbn1810@gmail.com>
2025-05-27 11:45:56 -0300

    User delete

--- src/main/java/com/sabino/curso/resource/UserResource.java
+++ src/main/java/com/sabino/curso/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sabino.curso.entities.User;
 import com.sabino.curso.services.UserService;
-
-import jakarta.servlet.Servlet;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -36,13 +35,18 @@ public class UserResource {
 	  User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	@PostMapping()
+	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		URI uir = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uir).body(obj);
 		
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
