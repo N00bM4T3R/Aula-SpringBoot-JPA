5d18c67b53f102136cf41f80d6fe15ff3753a757
N00bM4T3R <sabinolbn1810@gmail.com>
2025-05-30 17:25:15 -0300

    Exception handling - delete

--- src/main/java/com/sabino/curso/services/UserService.java
+++ src/main/java/com/sabino/curso/services/UserService.java
@@ -4,18 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sabino.curso.entities.User;
+import com.sabino.curso.repositories.CategoryRepositories;
 import com.sabino.curso.repositories.UserRepositories;
+import com.sabino.curso.services.exceptions.DatabaseException;
 import com.sabino.curso.services.exceptions.ResourceNotFoundException;
 
 
 @Service
 public class UserService {
+
+    private final CategoryRepositories categoryRepositories;
 	
   @Autowired
 	private UserRepositories repository;
+
+
+    UserService(CategoryRepositories categoryRepositories) {
+        this.categoryRepositories = categoryRepositories;
+    }
 	
   
   public List<User> findAll() {
@@ -32,8 +43,18 @@ public class UserService {
 	  
   }
   public void delete(Long id) {
-	  repository.deleteById(id);
-  }
+	  try {
+		  repository.deleteById(id);
+           }  
+	  catch (EmptyResultDataAccessException e) {
+	  throw new ResourceNotFoundException(id);
+}
+	  catch (DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
+		
+	}
+	  }
+ 
   public User update(Long id, User obj) {
 	  User entity = repository.getReferenceById(id);
 	  updateData(entity,obj);
